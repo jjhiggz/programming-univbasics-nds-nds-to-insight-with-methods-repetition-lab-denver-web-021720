dbd64723ba5f6e2cfb09e66f31133bf7b8575d41
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-28 22:55:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,6 +32,7 @@ def list_of_directors(source)
     list[i]=source[i][:name]
     i += 1
   end
+  return list
 end
 
 def total_gross(source)
@@ -44,4 +45,5 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
 end
